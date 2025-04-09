e686adec0eb05cb72564a09f701cc838c9f08e94
Marwane OUKACHA <oukachamarwane0@gmail.com>
2025-04-09 11:11:33 +0000

    first commit

---
+++ src/test/java/com/aitIsfoul/ait_isfoul_backend/AitIsfoulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitIsfoul.ait_isfoul_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitIsfoulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
